f7b88b772e2acc38d2a8e549c2d51ec323253889
lklegend <202011043@diu.iiitvadodara.ac.in>
2022-06-04 07:48:59 +0530

    Commit

---
+++ image_resizer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+from PIL import Image
+import os, sys
+
+path = "/Users/aashaysharma/Desktop/AnimeGan/images/final/"
+dirs = os.listdir( path )
+
+def resize():
+    for item in dirs:
+        if os.path.isfile(path+item):
+            im = Image.open(path+item)
+            f, e = os.path.splitext(path+item)
+            imResize = im.resize((64,64), Image.ANTIALIAS)
+            imResize.save(f + ' resized.jpg', 'JPEG', quality=90)
+
+resize()
